2bcfb5cedad03067708bc1d67dc38c06fb8ec543
Tuomas Reijonen <treijonen@live.fi>
2023-08-22 16:29:02 +0300

    Added demo calls to program.cs

--- ExceptionsAssignment/Program.cs
+++ ExceptionsAssignment/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Exceptions assignment");
+
+            Console.WriteLine("Assignment 1: Divide exception");
+            Console.WriteLine(ExceptionDemo.DivideWithException(10, 2));
+
+            Console.WriteLine("Assignment 2: Index out of bounds exception");
+            var array = new[] { "one", "two", "three" };
+            Console.WriteLine(ExceptionDemo.GetElementAtIndex(array, 1));
+
+            Console.WriteLine("Assignment 3: Argument null exception");
+            Console.WriteLine(ExceptionDemo.ArgumentNullExceptionDemo("123"));
+
+            Console.WriteLine("Assignment 4: Casting exception");
+            Console.WriteLine(ExceptionDemo.CastingExceptionDemo("test"));
+
+            Console.WriteLine("Assignment 5: Try catch");
+            Console.WriteLine(ExceptionDemo.TryCatchDemo("test"));
+
         }
     }
 }
